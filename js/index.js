ba30833c197ef965937f50a594ea7568efd71199
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-10-23 04:46:13 -0400

    added api request function

---
+++ js/index.js
@@ -0,0 +1,31 @@
+
+const hotRecipes = async (category) => {
+
+    let headers = new Headers([
+        ['Content-Type', 'application/json'],
+        ['Accept', 'application/json']
+    ]);
+
+    let request = new Request(`https://api.sampleapis.com/coffee/${category}`, {
+        method: 'GET',
+        headers: headers
+    });
+
+    const response = await fetch(request);
+    const json = await response.json();
+
+    let recipes = ``;
+    json.forEach((recipe) => {
+        recipes += `
+            <ul>
+                <li>${recipe.id}</li>
+                <li>${recipe.title}</li>
+                <li>${recipe.description}</li>
+                <li>${recipe.ingredients}</li>
+                <li>${recipe.image}</li>
+            </ul>
+        `
+    });
+
+    document.querySelector("p").innerHTML = recipes;
+}
